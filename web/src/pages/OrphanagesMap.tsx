3d9cd059248b222135076e2434fbfea3aaa0ba09
Wilson Neto <wilsonpsn@hotmail.com>
2020-10-15 19:36:18 -0300

    Commit for showing

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { FiPlus } from 'react-icons/fi';
+import { FiArrowRight, FiPlus } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import mapMarkerImg from '../images/map-marker.svg';
 import '../styles/pages/orphanages-map.css';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import Leaflet from 'leaflet';
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
 
 function OrphanagesMap() {
   return (
@@ -31,6 +39,18 @@ function OrphanagesMap() {
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
         />
+        <Marker icon={mapIcon} position={[-25.4481276, -49.2942842]} />
+        {/* <Popup
+          closeButton={false}
+          minWidth={240}
+          maxWidth={240}
+          // className="map-popup"
+        >
+          Lar Legal
+           <Link to="!">
+            <FiArrowRight size={20} color="#FFF" />
+          </Link> 
+        </Popup> */}
       </Map>
 
       <Link to="!" className="create-orphanage">
